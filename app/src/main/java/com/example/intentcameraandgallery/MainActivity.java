25a837fb08be1c2cfc40929ea6fafbdfd71e6dbe
Nguyen Dinh Sang <sangckm93@gmail.com>
2021-05-27 20:30:41 +0700

    Xử lý camera, load image from storage

---
+++ app/src/main/java/com/example/intentcameraandgallery/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.intentcameraandgallery;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mBtnCamera, mBtnGallery;
+    ImageView mImg;
+
+    int REQUEST_CODE_CAMERA = 123;
+    int REQUEST_CODE_GALLERY = 100;
+
+    ActivityResultLauncher<Intent> mActivityResultLauncherCamera;
+    ActivityResultLauncher<String> mActivityResultLauncherGallery;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnCamera = findViewById(R.id.buttonCamera);
+        mBtnGallery = findViewById(R.id.buttonGallery);
+        mImg = findViewById(R.id.imageView);
+
+        mActivityResultLauncherCamera = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+//                        Log.d("TEST", "here we go!");
+
+                        if(result.getData()!= null && result.getResultCode()== RESULT_OK){
+                            Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
+                            mImg.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+        );
+
+        mActivityResultLauncherGallery = registerForActivityResult(
+                new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        if (result != null) {
+                            mImg.setImageURI(result);
+                        }
+                    }
+                }
+        );
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Programmatically open permission specific
+//                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//                Uri uri = Uri.fromParts("package", getPackageName(), null);
+//                intent.setData(uri);
+//                startActivity(intent);
+
+                // Kiểm tra quyền camera đã được Granted
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CODE_CAMERA
+                    );
+                }else{
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    mActivityResultLauncherCamera.launch(intent);
+                }
+            }
+        });
+
+        mBtnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_GALLERY
+                    );
+                }else{
+                    mActivityResultLauncherGallery.launch("image/*");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_CAMERA){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                mActivityResultLauncherCamera.launch(intent);
+
+            }
+        }
+
+        if (requestCode == REQUEST_CODE_GALLERY){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                mActivityResultLauncherGallery.launch("image/*");
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
